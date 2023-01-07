1eb307a4c0ff38b464cbc7f4155f4be9e57e323f
julianmclain <julianrmclain@gmail.com>
2023-01-07 12:56:12 -0800

    Fix

--- draft.py
+++ draft.py
@@ -260,5 +260,4 @@ if __name__ == "__main__":
     players = get_player_analysis(draft_picks, yahoo_player_data)
     teams = get_players_by_team(players)
     team_differentials = get_team_differentials(players)
-    print(team_differentials)
     render_and_write_html(draft_picks, teams, players, team_differentials)
